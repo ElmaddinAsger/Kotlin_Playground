a1d7d4fe63f80ad7e81046d592aebc977c9d1cf8
Elmaddin Asadov <elmaddinasger@gmail.com>
2024-05-30 19:55:50 +0400

    Lesson 12

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main () {
-    println ("Welcom to my playground.")
+    println(taskOne(50))
+}
+
+//1.Faktoriali hesablayan rekursiv funksiya yazin
+fun taskOne (mult: Int) : Long {
+    return if (mult == 1) mult.toLong() else mult * taskOne(mult - 1)
+}
+
+//2.1-100 kimi ededlerin cemini hesablayan rekursiv funksiya yazin
+fun taskTwo (sum: Int) : Int {
+    return if (sum == 1) sum else sum + taskTwo(sum-1)
+}
+
+//3.1-99999 kimi ededlerin cemini hesablayan tail rekursiv funksiya yazin
+tailrec fun taskThree (num: Long, sum: Long) : Long {
+    return if (num == 1L) sum else taskThree(num-1,num+sum)
 }
