83418d9333d9b104c581c5d07654b5aeed6f19d0
Elmaddin Asadov <elmaddinasger@gmail.com>
2024-06-10 19:04:22 +0400

    Lesson 14

--- src/Main.kt
+++ src/Main.kt
@@ -24,7 +24,13 @@ fun main () {
     val myList4 = listOf(12,4589,34,555,66,78,13)
     println(taskTwo(myList4))
 
+    val myMap = mutableMapOf(1 to 12,3 to 46, 2 to 56 , 4 to 14, 5 to 37)
+    println(reload(myMap))
 
+
+    val taskFour = mutableListOf("asad", "jurnal","cut","bronze","item","engineer","android")
+    reList(taskFour)
+    println(taskFour)
 
 
 
@@ -161,11 +167,12 @@ class Collection (vararg input: String) {
 }
 
 
-//3.3.8 olculu mapListi verilb ve bu listde Key olarak studentId ve onun qiymeti saxlanilir
-//Hemin listi kıymete uygun olarak artan sirada nizamlayın
-//4.8 olculu string listi verilir,Hemin mentrleri birinci harflerine gore elifba sırası ile duzun.
 
 //1.8 olculu list verilir.Sade ededlerin cemini tapan
+fun taskOne (inputList: List<Int>) : Int {
+
+    return 5
+}
 
 //2.8 olculu list verilir.Elementin reqemlerinin cemi 10-dan boyuk olan ededlerin ededi ortasını tepin
 fun taskTwo (inputList: List<Int>) : Int {
@@ -191,6 +198,117 @@ fun taskTwo (inputList: List<Int>) : Int {
 
 }
 
+//3.3.8 olculu mapListi verilb ve bu listde Key olarak studentId ve onun qiymeti saxlanilir
+//Hemin listi kıymete uygun olarak artan sirada nizamlayın
+fun reload (inputMap: MutableMap<Int,Int>) : MutableMap <Int , Int> {
+    val resultMap = mutableMapOf<Int, Int>()
+    val sortList = mutableListOf<Int>()
+    for (i in inputMap.values){
+        sortList.add(i)
+    }
+
+
+    val size = sortList.size  //quickSort algoritm //val n = sortList.size
+    qucikSort1(sortList,0,size-1)  //quickSort(sortList, 0, n - 1)
+    println(sortList)
 
 
 
+
+
+    val keys = mutableListOf<Int>()
+    for (key in inputMap.keys){
+        keys.add(key)
+    }
+
+    for (value in sortList){
+        for (key in keys) {
+            if (inputMap[key] == value){
+                resultMap[key] = value
+                inputMap.remove(key)
+            }
+        }
+    }
+
+
+    return resultMap
+}
+fun qucikSort1 (sortList: MutableList<Int>,low: Int, hight: Int) {
+    if (low < hight) {
+     val point = point (sortList,low,hight)
+     qucikSort1(sortList,low,point-1)
+     qucikSort1(sortList,point+1,hight)
+    }
+}
+fun point (sortList: MutableList<Int>, low: Int, hight: Int) : Int {
+    var i = low-1
+    val pivot = sortList[hight]
+    for (n in low..hight){
+        if (sortList[n]<pivot){
+            i++
+            val temp = sortList[i]
+            sortList[i] = sortList[n]
+            sortList[n] = temp
+        }
+    }
+
+    val temp = sortList[i+1]
+    sortList[i+1] = sortList[hight]
+    sortList[hight] = temp
+    return i+1
+}
+/*fun quickSort(sortList: MutableList<Int>, low: Int, high: Int) {
+    if (low < high) {
+        val pi = partition(sortList, low, high)
+        quickSort(sortList, low, pi - 1)
+        quickSort(sortList, pi + 1, high)
+    }
+}
+
+fun partition(sortList: MutableList<Int>, low: Int, high: Int): Int {
+    val pivot = sortList[high]
+    var i = (low - 1)
+    for (j in low until high) {
+        if (sortList[j] < pivot) {
+            i++
+            val temp = sortList[i]
+            sortList[i] = sortList[j]
+            sortList[j] = temp
+        }
+    }
+    val temp = sortList[i + 1]
+    sortList[i + 1] = sortList[high]
+    sortList[high] = temp
+    return i + 1
+}*/
+ 
+//4.8 olculu string listi verilir,Hemin mentrleri birinci harflerine gore elifba sırası ile duzun.
+fun reList (mutableList: MutableList<String>) {
+    val size = mutableList.size
+    qucikSort(mutableList,0,size-1)
+
+}
+fun qucikSort (mutableList: MutableList<String>, low: Int,hight: Int) {
+    if (low < hight) {
+        val pivot = pivot(mutableList,low,hight)
+        qucikSort(mutableList,low,pivot-1)
+        qucikSort(mutableList,pivot+1,hight)
+    }
+}
+fun pivot (mutableList: MutableList<String>, low: Int,hight: Int) : Int {
+    var i = low-1
+    val point = mutableList[hight]
+    for (n in low..hight){
+        if ((mutableList[n])[0] <point[0]){
+            i++
+            val temp = mutableList[i]
+            mutableList[i] = mutableList[low]
+            mutableList[low] = temp
+        }
+    }
+
+    val temp = mutableList[i+1]
+    mutableList[i+1] = mutableList[hight]
+    mutableList[hight] = temp
+    return i+1
+}
